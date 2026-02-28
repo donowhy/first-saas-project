b7dfdd9eac46bd44e757efa983378229a0bb8485
강민승 <k4minseung@gmail.com>
2026-02-28 23:35:09 +0900

    feat(cross-platform): integrate backend and setup capacitor for mobile apps

---
+++ scheduleManagingFront/android/app/src/main/java/com/donowhy/schedulemanaging/MainActivity.java
@@ -0,0 +1,5 @@
+package com.donowhy.schedulemanaging;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ scheduleManagingFront/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.donowhy.schedulemanaging',
+  appName: 'ScheduleManaging',
+  webDir: 'dist'
+};
+
+export default config;
